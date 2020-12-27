b9c80c6ee07b79bfdb14c67a0ccc8004e792f349
madog <me@nicolascuadrado.com>
2020-12-26 21:19:56 -0500

    More Bootstrap JQuery

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
 $(document).ready(() => {
 
-    $('#my-carousel').carousel({ interval: 2000 })
-    $('#carousel-pause').click(() => {
-        $('#my-carousel').carousel('pause');
-    });
-    $('#carousel-play').click(() => {
-        $('#my-carousel').carousel('cycle');
+    const myCarousel = $('#my-carousel');
+    const carouselButton = $('#carousel-button');
+
+    myCarousel.carousel({ interval: 1000 })
+
+    carouselButton.click(() => {
+
+        if (carouselButton.children('span').hasClass('fa-pause')) {
+            myCarousel.carousel('pause');
+            carouselButton.children('span').removeClass('fa-pause');
+            carouselButton.children('span').addClass('fa-play');
+        } else {
+            myCarousel.carousel('cycle');
+            carouselButton.children('span').removeClass('fa-play');
+            carouselButton.children('span').addClass('fa-pause');
+        }
+
     });
 
-}); 
+});
